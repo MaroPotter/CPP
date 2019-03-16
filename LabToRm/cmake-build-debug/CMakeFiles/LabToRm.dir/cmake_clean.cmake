6e789c8fa6c62fed05d4803503f4a1bd0f55b0a0
MaroPotter <marek2105@vp.pl>
2019-03-16 19:45:49 +0100

    LabToRm

---
+++ LabToRm/cmake-build-debug/CMakeFiles/LabToRm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LabToRm.dir/main.cpp.obj"
+  "LabToRm.pdb"
+  "LabToRm.exe"
+  "LabToRm.exe.manifest"
+  "libLabToRm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LabToRm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
